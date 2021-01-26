3a0735bbec31fec94b98325ca9a21041c776d575
KamilHs <kamilsalimli1940@gmail.com>
2021-01-26 22:49:58 +0400

    ChatInput updated

--- src/modules/Home/components/Chat/ChatInput/ChatInput.tsx
+++ src/modules/Home/components/Chat/ChatInput/ChatInput.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { Box, makeStyles, Theme, TextField } from "@material-ui/core";
+import MicIcon from '@material-ui/icons/Mic';
 import SendIcon from '@material-ui/icons/Send';
+import CameraAltIcon from '@material-ui/icons/CameraAlt';
+import MoodIcon from '@material-ui/icons/Mood';
 
 const useStyles = makeStyles((theme: Theme) => (
     {
@@ -12,12 +15,14 @@ const useStyles = makeStyles((theme: Theme) => (
             padding: `${theme.spacing(1)}px ${theme.spacing(2)}px`,
         },
         textField: {
+            width: "100%",
             "& *::before,& *::after": {
                 display: "none"
             },
             "& input": {
                 width: "100%",
                 padding: theme.spacing(1),
+                paddingLeft: theme.spacing(4),
                 borderRadius: theme.shape.borderRadius,
                 color: theme.palette.secondary.main,
                 border: "1px solid #c6c6c6",
@@ -25,21 +30,49 @@ const useStyles = makeStyles((theme: Theme) => (
                     boxShadow: `0 0 8px 1px #c6c6c6`,
                 }
             },
-            flexGrow: 2,
-            marginRight: theme.spacing(1)
         },
-        sendIcon: {
+        textFieldContainer: {
+            flexGrow: 2,
+            marginRight: theme.spacing(1),
+            position: "relative"
+        },
+        icon: {
             cursor: "pointer"
+        },
+        emojis: {
+            position: "absolute",
+            left: "0",
+            top: "50%",
+            transform: "translateY(-50%)",
+            marginLeft: theme.spacing(0.5),
+        },
+        camera: {
+            marginRight: theme.spacing(1)
         }
     }
 ))
 
 export const ChatInput: React.FC = () => {
+    const [message, setMessage] = React.useState<string>("");
+    const handleChange = React.useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+        if (message === "")
+            setMessage(e.target.value.trim());
+        else
+            setMessage(e.target.value);
+    }, [message]);
     const classes = useStyles();
     return (
         <Box className={classes.container}>
-            <TextField className={classes.textField} />
-            <SendIcon color="primary" className={classes.sendIcon} />
+            <Box className={classes.textFieldContainer}>
+                <TextField onChange={handleChange} value={message} className={classes.textField} />
+                <MoodIcon color="disabled" className={[classes.icon, classes.emojis].join(" ")} />
+            </Box>
+            <CameraAltIcon color="primary" className={[classes.icon, classes.camera].join(" ")} />
+            {
+                message !== ""
+                    ? <SendIcon color="primary" className={classes.icon} />
+                    : <MicIcon color="primary" className={classes.icon} />
+            }
         </Box>
     )
 }
